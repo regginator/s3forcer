80dc0870162b130e8967a11347cf3da552565a25
regginator <reggie@latte.to>
2023-04-26 23:30:35 -0500

    <3

---
+++ README.md
@@ -0,0 +1,85 @@
+# s3forcer
+
+the funny setup.rbxcdn.com raw bruteforce grid thing:tm:
+
+## Install
+
+1. Clone the repository via `git`:
+
+    ```txt
+    git clone https://github.com/regginator/s3forcer.git
+    cd s3forcer
+    ```
+
+2. Install requirements ([`aiohttp`](https://pypi.org/project/aiohttp/)) via pip:
+
+    ```txt
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+`python3 main.py --help` (`py` instead of `python3` on Windows)
+
+```txt
+usage: main.py [-h] [--min-length MIN_LENGTH] [--chars CHARS] [--prefix PREFIX]
+               [--attempt-milestone ATTEMPT_MILESTONE] [--workers WORKERS]
+               [--webhook-url WEBHOOK_URL] [--reset-cache]
+
+s3 funny hacking
+
+options:
+  -h, --help            show this help message and exit
+  --min-length MIN_LENGTH
+                        Minimum character length of channel name combonations to bruteforce
+  --chars CHARS         Characters (in a single string) to use for channel name combonations
+  --prefix PREFIX       Prefix for channel names (e.g. "zfeature")
+  --attempt-milestone ATTEMPT_MILESTONE
+                        Number of attempts between cache writes and milestone prints
+  --workers WORKERS     Number of max thread workers to use
+  --webhook-url WEBHOOK_URL
+                        Discord webhook URL for found channel notifications
+  --reset-cache         Reset bin/cache_list.json
+```
+
+### Extra Notes
+
+* To quit the process and stop the request session, you need to use a keyboard interrupt (`Ctrl+C`) in your terminal, or just cancel the process directly through whatever you're using.
+
+* If you get immediate request errors, try toggling `--workers` to a number lower than the default of `250`! You may need to mess with this flag a bit to get the most desirable result for your own personal machine/network.
+
+* If you're annoyed at how often the "Milestone of (n) reached.." message is, you can toggle `--attempt-milestone` (which is the multiplier this uses) to a higher number! *This flag is also used for how often the session should write to the cache, which is what s3forcer uses to pick up (roughly) where you left off on a certain length cycle of your current prefix+char configuration, incase you exit the program or there's an uncaught error for whatever reason.*
+
+* Lastly, `--webhook-url` is optional, and accepts a string of a [Discord Webhook](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks) URL. When the session finds a channel not already in `bin/channels.json`, it'll send a `POST` request to the Webhook in question with the channel name, and DeployHistory link.
+
+<sup>
+
+*P.S., just try to figure everything else out yourself from the `--help` prompt; this is provided with zero extra support or warranty <3*
+
+</sup>
+
+## License
+
+```txt
+MIT License
+
+Copyright (c) 2023 Reggie <reggie@latte.to>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ main.py
@@ -0,0 +1,200 @@
+# s3forcer
+# reggie#1000 <3
+
+# enjoy the shit code.. I wrote this in like a day with little to no knowledge
+# about the libraries i'm using here lol. feel free to submit PRs with some
+# micro optimizations:tm:
+
+import os
+import asyncio
+import aiohttp
+import json
+from sys import exit as sys_exit
+from time import monotonic as time_monotonic
+from itertools import product as itertools_product
+from argparse import ArgumentParser
+
+# parse flags
+parser = ArgumentParser(description="s3 funny hacking")
+parser.add_argument("--min-length", type=int, default=1, help="Minimum character length of channel name combonations to bruteforce")
+parser.add_argument("--chars", type=str, default="abcdefghijklmnopqrstuvwxyz", help="Characters (in a single string) to use for channel name combonations")
+parser.add_argument("--prefix", type=str, default="z", help="Prefix for channel names (e.g. \"zfeature\")")
+parser.add_argument("--attempt-milestone", default=5000, type=int, help="Number of attempts between cache writes and milestone prints")
+parser.add_argument("--workers", type=int, default=250, help="Number of  max thread workers to use")
+parser.add_argument("--webhook-url", type=str, default=None, help="Discord webhook URL for found channel notifications")
+parser.add_argument("--reset-cache", action="store_true", help="Reset bin/cache_list.json")
+args = parser.parse_args()
+
+# read parsed flag args into dedicated variables
+min_length = args.min_length
+chars = args.chars
+prefix = args.prefix
+attempt_milestone = args.attempt_milestone
+workers = args.workers
+webhook_url = args.webhook_url
+reset_cache = args.reset_cache
+
+# -------------------------------------------------------- #
+
+def write_file(path, contents):
+    with open(path, "w") as file:
+        file.write(contents)
+        file.close()
+
+def read_file(path):
+    with open(path, "r") as file:
+        contents = file.read()
+        file.close()
+        return contents
+
+# create bin for stored db
+if not os.path.isdir("bin"):
+    os.mkdir("bin")
+
+# we'll check the --reset-cache flag before cont., which exits the prgm as-is
+if reset_cache:
+    write_file("bin/cache_list.json", "[]")
+    print("Reset bin/cache_list.json")
+    sys_exit()
+
+if not os.path.isfile("bin/channels.json"):
+    write_file("bin/channels.json", "[]")
+if not os.path.isfile("bin/cache_list.json"):
+    write_file("bin/cache_list.json", "[]")
+
+existing_cache_list = None
+def get_cache(): # returns cache_list, cache_positions
+    try:
+        cache_list = json.loads(read_file("bin/cache_list.json"))
+        existing_cache_list = cache_list
+    except:
+        cache_list = existing_cache_list
+
+    # get position cache for current cfg, if it exists
+    cache_table = None
+    for cache in cache_list:
+        if cache["chars"] == chars and cache["prefix"] == prefix:
+            cache_table = cache
+    if cache_table is None:
+        cache_table = {
+            "chars": chars,
+            "prefix": prefix,
+            "positions": {}
+        }
+
+        cache_list.append(cache_table)
+
+    cache_positions = cache_table["positions"]
+
+    return cache_list, cache_positions
+
+async def send_channel_req(session, combonation):
+    try:
+        channel_name = prefix + combonation
+        url = f"https://setup.rbxcdn.com/channel/{channel_name}/DeployHistory.txt"
+        async with session.head(url, timeout=15) as response:
+            if response.status == 200:
+                print(f"[+] Found Channel: {channel_name}")
+                channels = json.loads(read_file("bin/channels.json"))
+
+                if not channel_name in channels:
+                    channels.append(channel_name)
+                    try:
+                        write_file("bin/channels.json", json.dumps(channels, indent=4))
+                    except:
+                        pass
+
+                    # optional*
+                    if webhook_url:
+                        discord_response = await session.post(webhook_url, json={
+                            "embeds": [
+                                {
+                                    "title": f"Channel Found: **{channel_name}**",
+                                    "description": f"DeployHistory: <{url}>",
+                                    "color": 0x3498db,
+                                    "footer": {
+                                        "text": "s3forcer vee2 | reggie#1000 <3"
+                                    }
+                                }
+                            ]
+                        })
+
+                        if discord_response.status != 204 or discord_response.status != 200:
+                            error_msg = await discord_response.content.read()
+                            print(f"Discord Webhook error ({discord_response.status}): {error_msg}")
+    except asyncio.TimeoutError:
+        pass # ignore timeouts
+    except aiohttp.client_exceptions.ClientConnectionError:
+        pass
+    except aiohttp.client_exceptions.ServerDisconnectedError:
+        pass
+    except Exception as err:
+        # :middle_finger:
+        print(f"[!] Unhandled error exception on aiohttp (send_channel_req): {err}")
+
+async def main():
+    # no tcp connection limit
+    connector = aiohttp.TCPConnector(limit=None)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        # we need to limit task queing at a time
+        semaphore = asyncio.Semaphore(workers)
+
+        char_len = len(chars)
+
+        # go thru each length combination progressively
+        length = min_length
+        while True:
+            print(f"---- STARTING ATTEMPTS @ {length} CHARS (MAX ATTS: {pow(char_len, length)}) ----")
+            start_time = time_monotonic()
+
+            # we WON'T list everything into an iter directly, we'll go through it properly..
+            channel_names = map("".join, itertools_product(chars, repeat=length))
+
+            # we'll manage ongoing tasks ourselves
+            current_attempt = 1
+
+            cache_list, cache_positions = get_cache()
+
+            length_str = str(length)
+            if length_str in cache_positions:
+                cache_position = cache_positions[length_str]
+                print(f"^^ STARTING AT CACHED POSITION OF {cache_position}.. ^^")
+                for _ in channel_names:
+                    if current_attempt > cache_position:
+                        break
+                    current_attempt += 1
+
+            for channel_name in channel_names:
+                if current_attempt % attempt_milestone == 0:
+                    print(f"Milestone of {current_attempt} attempts reached..")
+                    
+                    cache_list, cache_positions = get_cache()
+                    cache_positions[length_str] = current_attempt
+                    try:
+                        write_file("bin/cache_list.json", json.dumps(cache_list, indent=4))
+                    except:
+                        pass
+
+                await semaphore.acquire()
+                task = asyncio.ensure_future(send_channel_req(session, channel_name))
+                task.add_done_callback(lambda _: semaphore.release())
+
+                current_attempt += 1
+
+            # we're not using asyncio.gather with a task tracker at the end of a length cycle, as
+            # it absolutely KILLS memory usage in the process lol
+
+            end_time = time_monotonic()
+            print(f"^^ Cycle for @{length} chars took: {end_time - start_time:.3f} seconds ^^")
+
+            # increase the len for the next cycle!
+            length += 1
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt received, exiting..")
+    sys_exit()
+except Exception as err:
+    print(f"[CRITICAL EXIT] Unhandled exception: {err}")
+    sys_exit()
